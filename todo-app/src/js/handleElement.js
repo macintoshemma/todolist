b2b21a1d710701eaafe6ad4dd5e2bf71de772baf
Emma Macintosh <emma.macintosh@appliedtechnology.se>
2019-04-08 14:20:14 +0200

    fixed statuschange of todos

--- todo-app/src/js/handleElement.js
+++ todo-app/src/js/handleElement.js
@@ -1,14 +1,21 @@
-const moveElement = (event) => {
-  const listContainer = event.target.parentNode;
-  const itemId = listContainer.id;
-  const state = localStorage.getItem(`state${itemId}`);
-  const completeSection = document.getElementsByClassName('complete')[0];
+const moveElement = (e) => {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  const complete = document.getElementsByClassName('complete')[0];
+  const currentTodo = e.target.parentNode;
+  const { id } = currentTodo;
+  const completeBtn = document.getElementsByClassName('complete__btn')
 
-  if (state === 'todo') {
-    localStorage.setItem(`state${itemId}`, 'complete');
-    listContainer.parentNode.removeChild(listContainer);
-    completeSection.appendChild(listContainer);
-  }
+  todos.forEach((todo) => {
+    if (todo.id == id) {
+      todo.complete = true;
+    }
+
+    if (todo.complete == true) {
+      currentTodo.parentNode.removeChild(currentTodo);
+      complete.appendChild(currentTodo);
+      completeBtn.textContent = 'Undo';
+    }
+  });
 };
 
 const removeElement = (event) => {
@@ -21,3 +28,14 @@ const removeElement = (event) => {
 };
 
 module.exports = { moveElement, removeElement };
+
+// const listContainer = event.target.parentNode;
+// const itemId = listContainer.id;
+// const state = localStorage.getItem(`state${itemId}`);
+// const completeSection = document.getElementsByClassName('complete')[0];
+// console.log(event.target);
+// if (state === 'todo') {
+//   localStorage.setItem(`state${itemId}`, 'complete');
+//   listContainer.parentNode.removeChild(listContainer);
+//   completeSection.appendChild(listContainer);
+// }
